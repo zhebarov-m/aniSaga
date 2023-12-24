b9b1a624dcdb220a60dd5a1973a0dbd1fb79bfce
xarcho.ev <xarcho.ev@gmail.com>
2023-12-24 03:16:22 +0300

    добавил роутинг и разбил бандл на чанки для ленивой подгрузки

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import Test from "./components/Test";
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {FaqPageAsync} from "./pages/FAQPage/FaqPage.async";
 
 const App = () => {
     return (
         <div className="App">
-            <Test />
+            <Link to="/">Главная</Link>
+            <Link to="/faq">Вопросы</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<MainPageAsync/>}/>
+                    <Route path="/faq" element={<FaqPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
