b545653fea1eac4649882248d5191055ded7d7b4
xarcho.ev <xarcho.ev@gmail.com>
2024-01-08 19:20:22 +0300

    добавил перевод на ссылки в Navbar.tsx

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,22 @@
 import { customClasses } from 'shared/lib/classNames/customClasses';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={customClasses(styles.Navbar, {}, [className!])}>
-        <div className={styles.links}>
-            <AppLink theme={AppLinkTheme.PRIMARY} className={styles.mainLink} to="/">Главная</AppLink>
-            <AppLink theme={AppLinkTheme.PRIMARY} className={styles.faqLink} to="/faq">Вопросы</AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={customClasses(styles.Navbar, {}, [className!])}>
+            <div className={styles.links}>
+                <AppLink theme={AppLinkTheme.PRIMARY} className={styles.mainLink} to="/">{t('Главная')}</AppLink>
+                <AppLink theme={AppLinkTheme.PRIMARY} className={styles.faqLink} to="/faq">{t('Вопросы')}</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
